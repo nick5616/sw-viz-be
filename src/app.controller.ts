58debf3f3a4b49757fcb522a9ce63e7cf095591f
nick5616 <nickbelovoskey@gmail.com>
2023-11-29 23:52:32 -0800

    add `/cost/` endoint to app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Get('/film/:filmNumber')
-  getStarShipsInFilm(@Param('filmNumber') filmNumber: number) {
-    return this.appService.getStarshipsInFilm(filmNumber);
-  }
-
-  @Get('/cost/:filmNumber')
-  getCostOfFilm(@Param('filmNumber') filmNumber: number) {
-    return this.appService.getCostOfStarshipsInFilm(filmNumber);
+  @Get('/cost/')
+  getCostOfAll() {
+    return this.appService.getCostOfAllStarships();
   }
 }
